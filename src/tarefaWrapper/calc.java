db6865d1c3587677d8c6d434581b85c91613cf7a
unknown <you@example.com>
2024-09-26 10:08:51 -0300

    primeiro commit

---
+++ src/tarefaWrapper/calc.java
@@ -0,0 +1,14 @@
+package tarefaWrapper;
+
+public class calc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 10;
+		Integer number1 = num1;
+		
+		System.out.println(number1);
+		
+	}
+
+}
